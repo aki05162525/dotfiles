ffb23d88ee99978653b7e9163196e430abc254a1
aki05162525 <akihiro05162525@gmail.com>
2026-04-24 19:32:56 +0900

    add shell aliases

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,6 +29,11 @@
 
   programs.zsh = {
     enable = true;
+    shellAliases = {
+      zj = "zellij";
+      ll = "ls -la";
+      g  = "git";
+    };
     envExtra = ''
       export PATH="$HOME/.local/bin:$HOME/.bun/bin:$PATH"
     '';
