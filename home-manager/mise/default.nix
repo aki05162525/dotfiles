afbbb838d0a0ab231e6cac848d3c64e5bb066281
aki05162525 <akihiro05162525@gmail.com>
2026-07-02 17:26:01 +0900

    perf: mise ビルド時の cargo test を無効化する
    nixpkgs の mise は doCheck=true で毎回 rattler 等の重い
    dev-dependencies までコンパイルされビルドが遅いため、
    日常使いのCLIには不要なテストをスキップする。

--- home-manager/mise/default.nix
+++ home-manager/mise/default.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
+    # nixpkgs の mise は checkPhase で cargo test を走らせる(doCheck = true)ため、
+    # rattler 等の重い dev-dependencies まで毎回コンパイルされてビルドが非常に遅くなる。
+    # 日常使いのCLIとしてはテスト不要なので無効化する。
+    package = pkgs.mise.overrideAttrs (_: {
+      doCheck = false;
+    });
     globalConfig = {
       tools = {
         node = "24";
